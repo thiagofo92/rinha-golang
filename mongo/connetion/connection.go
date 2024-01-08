13a3cb4b90ceb291b168feab46bf81429afec32e
thiagofo92 <thiagofo92@gmail.com>
2024-01-08 09:37:01 -0300

    Update connection.go

--- mongo/connetion/connection.go
+++ mongo/connetion/connection.go
@@ -9,7 +9,6 @@ import (
 )
 
 func NewConnectionMongo() (*mongo.Client, error) {
-	// uri := "mongodb+srv://root:12344321@personal.q6co5ue.mongodb.net/?retryWrites=true&w=majority"
 	uri := "mongodb://root:12344321@localhost:27017/?maxPoolSize=20&w=majority"
 	ctx, cancel := context.WithTimeout(context.Background(), 3*time.Second)
 	defer cancel()
